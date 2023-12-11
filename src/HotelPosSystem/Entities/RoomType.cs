30bd656b0540818a6a8d71aefb7d8ae32eb63c46
Simon Clavensjö <simon@clavensjo.se>
2023-12-11 11:13:38 +0100

    Make RoomType.Name required
    Co-Authored-By: Andreas423 <142991086+Andreas423@users.noreply.github.com>
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>
    Co-Authored-By: Leo Ahlinder <59998695+LeoAhlinder@users.noreply.github.com>

--- src/HotelPosSystem/Entities/RoomType.cs
+++ src/HotelPosSystem/Entities/RoomType.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelPosSystem.Entities {
     internal class RoomType {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
     }
 }
