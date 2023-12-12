62970161d4ddbaf54f0b798ec1c9f32c4e90c91f
Simon Clavensjö <simon@clavensjo.se>
2023-12-12 11:30:28 +0100

    Allow specifying database path as CLI argument
    Co-Authored-By: Andreas423 <142991086+Andreas423@users.noreply.github.com>
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>
    Co-Authored-By: Leo Ahlinder <59998695+LeoAhlinder@users.noreply.github.com>

--- src/HotelPosSystem/Program.cs
+++ src/HotelPosSystem/Program.cs
@@ -1,10 +1,20 @@
 namespace HotelPosSystem {
     internal static class Program {
+        internal static string? DatabasePath;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static void Main() {
+        private static void Main(string[] args) {
+            if (args.Length > 0) {
+                DatabasePath = args[0];
+            } else {
+                Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
+                string path = Environment.GetFolderPath(folder);
+                DatabasePath = Path.Join(path, "hotelPosSystem.db");
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
