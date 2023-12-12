2c3a9310060e9e49dc5bfc3f979b618173b35014
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-12-12 14:59:50 +0100

    Update variable names for clarity
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>
    Co-Authored-By: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>
    Co-Authored-By: Andreas423 <142991086+Andreas423@users.noreply.github.com>

--- src/HotelPosSystem.Tests/ProgramFixture.cs
+++ src/HotelPosSystem.Tests/ProgramFixture.cs
@@ -4,26 +4,26 @@ namespace HotelPosSystem.Tests {
     public class ProgramFixture : IDisposable {
         public readonly Application Application;
 
-        private readonly string _testDatabaseCopyPath = string.Empty;
+        private readonly string _temporaryTestDatabasePath = string.Empty;
 
         public ProgramFixture() {
             string srcDirectoryPath = "../../../../";
-            string executablePath = "HotelPosSystem/bin/Release/net8.0-windows/win-x64/HotelPosSystem.exe";
-            string path = Path.Combine(srcDirectoryPath, executablePath);
+            string executablePathFromSrc = "HotelPosSystem/bin/Release/net8.0-windows/win-x64/HotelPosSystem.exe";
+            string executablePath = Path.Combine(srcDirectoryPath, executablePathFromSrc);
             string testDatabasePath = Path.Combine(srcDirectoryPath, "HotelPosSystem.Tests/testData.db");
 
             // Run tests on a copy of the database to ensure that the data stays the same
             // The copy is discarded after the tests have finished running
-            _testDatabaseCopyPath = testDatabasePath + ".temp";
-            File.Copy(testDatabasePath, _testDatabaseCopyPath);
+            _temporaryTestDatabasePath = testDatabasePath + ".temp";
+            File.Copy(testDatabasePath, _temporaryTestDatabasePath);
 
-            Application = Application.Launch(path, _testDatabaseCopyPath);
+            Application = Application.Launch(executablePath, _temporaryTestDatabasePath);
         }
 
         public void Dispose() {
             Application.Close();
             Application.Dispose();
-            File.Delete(_testDatabaseCopyPath);
+            File.Delete(_temporaryTestDatabasePath);
         }
     }
 }
