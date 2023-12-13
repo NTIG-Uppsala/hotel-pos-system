176678ab2f6237b18919b9000c765d802248d117
Andreas423 <142991086+Andreas423@users.noreply.github.com>
2023-12-13 15:29:48 +0100

    List bookings
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>
    Co-Authored-By: Leo Ahlinder <59998695+LeoAhlinder@users.noreply.github.com>
    Co-Authored-By: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>

---
+++ src/HotelPosSystem.Tests/BookingTests.cs
@@ -0,0 +1,107 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+
+namespace HotelPosSystem.Tests {
+    public class BookingTests : IDisposable, IClassFixture<ProgramFixture> {
+        private readonly UIA3Automation _automation;
+        private readonly ProgramFixture _fixture;
+
+        private const string BookingListAutomationId = "bookingList";
+
+        public BookingTests(ProgramFixture fixture) {
+            _automation = new UIA3Automation();
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void CustomerNameShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label customerName = GetElement("customerName1", bookingList).AsLabel();
+
+            Assert.Contains("Robert Robertsson", customerName.Text);
+        }
+
+        [Fact]
+        public void EmailAdressShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label emailAddress = GetElement("emailAddress2", bookingList).AsLabel();
+
+            Assert.Contains("robert.robertsson@example.com", emailAddress.Text);
+        }
+
+        [Fact]
+        public void PhoneNumberShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label phoneNumber = GetElement("phoneNumber3", bookingList).AsLabel();
+
+            Assert.Contains("070-1740640", phoneNumber.Text);
+        }
+
+        [Fact]
+        public void RoomNameShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label roomName = GetElement("roomName1", bookingList).AsLabel();
+
+            Assert.Contains("202", roomName.Text);
+        }
+
+        [Fact]
+        public void DatesShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label startEndDatesLabel = GetElement("dates2", bookingList).AsLabel();
+
+            Assert.Contains("2023-12-27", startEndDatesLabel.Text);
+            Assert.Contains("2024-01-03", startEndDatesLabel.Text);
+        }
+
+        [Fact]
+        public void CheckedInStatusShouldBeCorrect() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            CheckBox checkedInCheckBox = GetElement("checkedIn1", bookingList).AsCheckBox();
+            CheckBox notCheckedInCheckBox = GetElement("checkedIn2", bookingList).AsCheckBox();
+
+            Assert.True(checkedInCheckBox.IsChecked);
+            Assert.False(notCheckedInCheckBox.IsChecked);
+        }
+
+        [Fact]
+        public void PaidStatusShouldBeCorrect() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            CheckBox paidCheckBox = GetElement("paidFor2", bookingList).AsCheckBox();
+            CheckBox notPaidCheckBox = GetElement("paidFor1", bookingList).AsCheckBox();
+
+            Assert.True(paidCheckBox.IsChecked);
+            Assert.False(notPaidCheckBox.IsChecked);
+        }
+
+        [Fact]
+        public void CommentShouldBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            Label commentLabel = GetElement("comment1", bookingList).AsLabel();
+
+            Assert.Contains("Cleaning crew one hour late", commentLabel.Text);
+        }
+
+        [Fact]
+        public void CommentShouldNotBePresentInList() {
+            AutomationElement bookingList = GetElement(BookingListAutomationId, GetMainWindow());
+            AutomationElement? commentLabel = GetElement("comment2", bookingList);
+
+            Assert.Null(commentLabel);
+        }
+
+        private AutomationElement GetElement(string automationId, AutomationElement parent) {
+            ConditionFactory conditionFactory = _automation.ConditionFactory;
+            return parent.FindFirstDescendant(conditionFactory.ByAutomationId(automationId));
+        }
+
+        private Window GetMainWindow() {
+            return _fixture.Application.GetMainWindow(_automation).AsWindow();
+        }
+
+        public void Dispose() {
+            _automation.Dispose();
+        }
+    }
+}
--- src/HotelPosSystem/MainForm.cs
+++ src/HotelPosSystem/MainForm.cs
@@ -1,4 +1,8 @@
+using System.Globalization;
+
 using HotelPosSystem.Entities;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelPosSystem {
     public partial class MainForm : Form {
@@ -7,6 +11,88 @@ namespace HotelPosSystem {
 
             using HotelDbContext databaseContext = new();
             DatabaseUtilities.SetUpDatabase(databaseContext);
+
+            FlowLayoutPanel bookingList = new() {
+                Name = "bookingList",
+                FlowDirection = FlowDirection.TopDown,
+                AutoScroll = true,
+                WrapContents = false,
+                Dock = DockStyle.Fill
+            };
+
+            Booking[] test = databaseContext.Bookings.ToArray();
+
+            Booking[] bookings = databaseContext.Bookings
+                .Include(booking => booking.Customer)
+                .Include(booking => booking.Room)
+                .OrderBy(booking => booking.StartDate)
+                .ToArray();
+
+            foreach (Booking booking in bookings) {
+                bookingList.Controls.Add(CreateListItem(booking));
+            }
+            Controls.Add(bookingList);
+        }
+
+        private static FlowLayoutPanel CreateListItem(Booking booking) {
+            FlowLayoutPanel flowLayoutPanel = new() {
+                FlowDirection = FlowDirection.TopDown,
+                AutoSize = true,
+                Margin = new Padding(0, 0, 0, bottom: 40)
+            };
+
+            AddLabel(flowLayoutPanel, "customerName" + booking.Id, "Name: " + booking.Customer.FullName);
+            AddLabel(flowLayoutPanel, "emailAddress" + booking.Id, "Email address: " + booking.Customer.EmailAdress);
+            AddLabel(flowLayoutPanel, "phoneNumber" + booking.Id, "Phone number: " + booking.Customer.PhoneNumber);
+            AddLabel(flowLayoutPanel, "roomName" + booking.Id, "Room: " + booking.Room.Name);
+
+            // "o" date format corresponds to the ISO 8601 standard
+            // https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+            string startDate = booking.StartDate.ToString("o", CultureInfo.InvariantCulture);
+            string endDate = booking.EndDate.ToString("o", CultureInfo.InvariantCulture);
+            AddLabel(flowLayoutPanel, "dates" + booking.Id,
+                               $"Dates: {startDate} to {endDate}");
+
+            AddCheckBoxAndLabel(flowLayoutPanel, "paidFor" + booking.Id, booking.IsPayedFor, "", "Has paid:");
+            AddCheckBoxAndLabel(flowLayoutPanel, "checkedIn" + booking.Id, booking.IsCheckedIn, "", "Has checked in:");
+
+            if (booking.Comment is not null) {
+                AddLabel(flowLayoutPanel, "comment" + booking.Id, "Comment: " + booking.Comment);
+            }
+
+            return flowLayoutPanel;
+        }
+
+        private static void AddCheckBoxAndLabel(Panel container, string checkBoxName, bool checkBoxState, string labelName, string labelText) {
+            FlowLayoutPanel layoutPanel = new() {
+                FlowDirection = FlowDirection.LeftToRight,
+                AutoSize = true
+            };
+
+            AddLabel(layoutPanel, labelName, labelText);
+            AddCheckBox(layoutPanel, checkBoxName, checkBoxState, isEnabled: false);
+
+            container.Controls.Add(layoutPanel);
+        }
+
+        private static void AddLabel(Panel container, string name, string text) {
+            Label label = new() {
+                Name = name,
+                Text = text,
+                UseCompatibleTextRendering = true,
+                AutoSize = true
+            };
+            container.Controls.Add(label);
+        }
+
+        private static void AddCheckBox(Panel container, string name, bool isChecked, bool isEnabled) {
+            CheckBox checkBox = new() {
+                Name = name,
+                Checked = isChecked,
+                Enabled = isEnabled,
+                UseCompatibleTextRendering = true
+            };
+            container.Controls.Add(checkBox);
         }
 
         private static Button CreateButton(string name, string text, EventHandler? onClicked) {
