50b0e9c5bab1e78fc479ba7fbf8af8e0e01ce7f2
Simon Clavensjö <simon@clavensjo.se>
2024-01-22 10:26:01 +0100

    Revert "Make app fullscreen on start"
    This reverts commit 91e4090741abad2e34b50bafac6dbe8bbb0c9229.

--- src/HotelPosSystem/MainForm.Designer.cs
+++ src/HotelPosSystem/MainForm.Designer.cs
@@ -25,7 +25,7 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.WindowState = FormWindowState.Maximized;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Hotel POS System";
         }
 
